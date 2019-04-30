91e665cc61114bee8f0b4ff4a0cb06486f35eba1
Vinoth Kumar Nanda Kumar <vinothkumar.nk@gmail.com>
2019-04-29 20:56:58 -0500

    Final commit

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,32 +1,42 @@
 //1. Write a JavaScript program to display the current day and time.
 console.log(Date())
+document.getElementById("date").innerHTML = Date();
 
 //2. Write a JavaScript program to convert a number to a string.
 var num = 15;
 var n = num.toString();
 console.log(n);
+document.getElementById("numtostr").innerHTML = n;
 
 //3. Write a JavaScript program to convert a string to the number.
 var a = parseInt("10");
 console.log(a);
+document.getElementById("strtonum").innerHTML = a;
 
 //5. Write a JavaScript program that adds 2 numbers together.
 var a = 10;
 var b= 15;
-console.log(a+b);
+var z = a+b;
+console.log(z);
+document.getElementById("add2num").innerHTML = z;
+
 
 //6. Write a JavaScript program that runs only when 2 things are true.
 var a = 5;
 var b = 5;
 if (a===b){
     console.log("True");
+    document.getElementById("run2true").innerHTML = "True";
 }
+
+
 
 //7. Write a JavaScript program that runs when 1 of 2 things are true.
 var a = 5;
 var b = 5;
 if (a > b || a==b){
     console.log("True");
+    document.getElementById("run1true").innerHTML = "True";
 }
 
 
@@ -35,8 +45,9 @@ var a = 1;
 var b = 9;
 if (a > b || a==b){
     console.log("True");
+    document.getElementById("runnottrue").innerHTML = "True";
 }
-else console.log("false");
+else { console.log("false"); document.getElementById("runnottrue").innerHTML = "false"; }
 
 
 
@@ -47,6 +58,12 @@ var u = undefined;
 var p = 1;
 var a = "b";
 
+document.getElementById("bool").innerHTML = typeof f;
+document.getElementById("null").innerHTML = typeof c;
+document.getElementById("undef").innerHTML = typeof u;
+document.getElementById("num").innerHTML = typeof p;
+document.getElementById("notnum").innerHTML = isNaN(p);
+document.getElementById("str").innerHTML = typeof a;
 
 console.log(typeof f);
 console.log(typeof c);
