92f762974c61f010e6449cbd093da6f1786d0c3f
Vinoth Kumar Nanda Kumar <vinothkumar.nk@gmail.com>
2019-05-12 11:35:46 -0500

    Commit for ticTacToe

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -25,27 +25,63 @@ function printBoard() {
 
 function horizontalWin() {
   // Your code here
+  if(board[0][0]=='X' && board[0][1]=='X' && board[0][2]=='X')
+    return true;
+
+  else
+  return false;  
+    
 }
 
 function verticalWin() {
   // Your code here
-}
+  if(board[0][1]=='X' && board[1][1]=='X' && board[2][1]=='X')
+    return true;
+   else
+   return false;
+  }
 
 function diagonalWin() {
   // Your code here
+  if(board[0][0]=='X' && board[1][1]=='X' && board[2][2]=='X')
+    return true;
+  else 
+  return false;
 }
 
 function checkForWin() {
   // Your code here
+
+  if(horizontalWin() == true || verticalWin() == true || diagonalWin() == true)
+    return true;
+  else
+  return false;
+  // }
+  // else if(verticalWin() == true){
+  //   return true;
+  // }
+  // else if(diagonalWin() == true){
+  //   return true;
+  // }
 }
 
 function ticTacToe(row, column) {
   // Your code here
+  
+  if (playerTurn === 'X'){
+    board[row][column]='X';
+    playerTurn = "O";
+  }
+
+  else{
+    board[row][column]='O';
+    playerTurn = "X";
+  }
 }
 
 function getPrompt() {
   printBoard();
-  console.log("It's Player " + playerTurn + "'s turn.");
+   console.log("It's Player " + playerTurn + "'s turn.");
   rl.question('row: ', (row) => {
     rl.question('column: ', (column) => {
       ticTacToe(row, column);
