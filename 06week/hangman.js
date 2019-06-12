21af23f1f689c193b50edecabf5b1be1cef30dfc
loriannehutauruk <lori@hutauruk.com>
2019-06-12 18:36:35 -0500

    adding progress to hangman - still need check for win function, a counter for wrong, and the dom

--- 06week/hangman.js
+++ 06week/hangman.js
@@ -8,41 +8,61 @@ output: process.stdout
 });
 
 
-var word = 'Hello';
+var word = 'hello';
 var arr = word.split('');
 var solved = ['_','_','_','_','_'];
 var maxLives = 5;
-var counter=0;
+var right = 0;
+var wrong = 0;
 var letter = '';
 
 function getPrompt() {
 
-    rl.question('which letter?: ', (letter) => {
+rl.question('which letter?: ', (letter) => {
 
     hangman(letter);
+    // wrongLetter();
+
     console.log(solved);
-        if(counter < 5){
-        getPrompt();
-        }
-        else{
-        console.log("GAME OVER SUCKER");
-        }
+    //print solved
+    // document.getElementById('solved').innerHTML = solved;
+    // checkForWin(arr, solved);
+    if(wrong < 5) {
+    getPrompt();
+    }
 
-    });
+}
 
-};
+)}
 
-function hangman(letter){
+function hangman(letter) {
 
 //Loop through the word and find the letter that matches the prompt
-    for (let i=0;i<arr.length;i++){
+for (let i=0;i<arr.length;i++) {
 
-        if(letter === arr[i] ){
-        solved[i] = letter;
-        }
+    if(letter === arr[i]) {
+    solved[i] = letter;
+    right++;
+    console.log(right);
     }
-counter++;
-}
-getPrompt();
 
-// compare the arrays first, if the arrays are equal to each other, then you win. Create new function called checkForWin
+}
+
+}
+
+// function checkForWin(arr, solved) {
+//     if(arr === solved) {
+//     console.log("YOU WIN YOU STUD");
+//     }
+//     else if(wrong > 5) {
+//     console.log("GAME OVER SUCKER");
+//     }
+// }
+
+// function wrongLetter() {
+//     if (hangman(letter) == false) {
+//     wrong++;
+//     console.log(wrong);
+//     }
+// }
+getPrompt();
