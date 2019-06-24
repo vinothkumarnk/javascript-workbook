bc33b2a6f4671fed54e52e0e6ba57ea2a4988b41
Vinoth Kumar Nanda Kumar <vinothkumar.nk@gmail.com>
2019-06-24 07:20:10 -0500

    fetch

--- 07week/Fetchapi/main.js
+++ 07week/Fetchapi/main.js
@@ -1,31 +1,32 @@
-let arrayOfPosts;
+let arrayOfUsers;
 
 // this function waits for the web page to be loaded, when it does it will run the code inside of it which happen to be getPosts()
 window.onload = function() {
-  getPosts()
+  getUsers()
 
 }
 
-// this function is going to make a fetch request to the url inside it's parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
-const getPosts = () => {
-  fetch('https://randomuser.me/api/')
-    .then(res => res.json())
-    .then(posts => arrayOfPosts = posts)
-    //.then(posts =>console.log(posts))
+const getUsers = () => {
+    fetch('https://randomuser.me/api/')
+      .then(res => res.json())
+      .then(x => arrayOfUsers = x['results'])
+     // .then(users => arrayOfUsers,push(users))
+
 }
+
 
 // this function logs the results in your browsers console
-const consolePosts = () => {
-  console.log(arrayOfPosts)
+const consoleUsers = () => {
+  console.log(arrayOfUsers)
 }
 
 // this function creates elements inside the all-posts ul, then appends text inside it with the posts that were returned in the request.
-const displayPost = () => {
-  const allPosts = document.getElementById('all-posts')
-  arrayOfPosts.map((post, index) => {
+const displayUsers = () => {
+  const allUsers = document.getElementById('all-users')
+  arrayOfUsers.map((x, index) => {
     const li = document.createElement('li')
-    const text = document.createTextNode(`#${index}, Title: ${post.title}:  ${post.body}, by user: ${post.userId}`)
+    const text = document.createTextNode(`#${index}, Title: ${results['name']['first']}:  ${results.gender}, by user: ${resulsts.location}`)
     li.appendChild(text)
-    allPosts.append(li)
+    allUsers.append(li)
   })
 }
